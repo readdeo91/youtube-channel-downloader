718e4756627feeea80f60a4baad88397d601c05d
richardMeggyes <readdeo91@gmail.com>
2023-04-24 17:21:44 +0200

    Added spring

---
+++ src/main/java/hu/readdeo/youtube/channel/downloader/YoutubeChannelDownloaderApplication.java
@@ -0,0 +1,13 @@
+package hu.readdeo.youtube.channel.downloader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoutubeChannelDownloaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoutubeChannelDownloaderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/readdeo/youtube/channel/downloader/YoutubeChannelDownloaderApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.readdeo.youtube.channel.downloader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoutubeChannelDownloaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
